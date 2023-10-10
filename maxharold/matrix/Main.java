f71dc551d730cb16e6b7bcf400d5962cfe612d13
Juan Diaz <22diazj@cistercian.org>
2023-10-10 11:13:18 -0500

    removed docs and made jar file

---
+++ maxharold/matrix/Main.java
@@ -0,0 +1,7 @@
+package maxharold.matrix;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
--- out/production/SimpleMatrixLibrary/docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"maxharold.matrix","l":"Matrix"},{"p":"maxharold.matrix","l":"RotUtil"},{"p":"maxharold.matrix","l":"Vector"}];updateSearchResults();
